3dce54572798a8ef4f4773acd558b3c7cb53c858
Leo Moraes <moraes.leo@gmail.com>
2020-11-05 12:10:31 -0300

    Alteracao da pagina conforme o json

---
+++ src/components/ListItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { User } from '../interfaces'
+
+type Props = {
+  data: User
+}
+
+const ListItem = ({ data }: Props) => (
+  <Link href="/users/[id]" as={`/users/${data.id}`}>
+      <a>{data.id}:  
+      <span> {data.first_name} {data.last_name} </span> 
+      ({data.linkedin})
+      </a>
+  </Link>
+)
+
+export default ListItem
